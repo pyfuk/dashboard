57ab4428d043a2bae3b994783bf0d8ab46baae31
Shakhboz_Khalikov <khalikov.shakhboz@payme.uz>
2022-09-30 18:23:10 +0500

    added validation for subject form

--- src/views/components/Subjects/AddSubjectForm.vue
+++ src/views/components/Subjects/AddSubjectForm.vue
@@ -8,20 +8,26 @@
       >{{ $t('subjects.name_ru') }}</label
       >
       <argon-input v-model="form.name.ru" type="text"
-                   :placeholder="$t('subjects.name_ru')"/>
+                   :placeholder="$t('subjects.name_ru')"
+                   :valid="this.validate(formSubmitted, v$.form.name.ru.$error)"
+                   :valid-text="this.validateText(v$.form.name.ru)"
+      />
 
-      <label :for=" form.name.uz
-      " class="form-control-label"
-      >{{ $t('subjects.name_uz') }}</label
-      >
+      <label :for=" form.name.uz" class="form-control-label"
+      >{{ $t('subjects.name_uz') }}</label>
       <argon-input v-model="form.name.uz" type="text"
-                   :placeholder="$t('subjects.name_uz')"/>
+                   :placeholder="$t('subjects.name_uz')"
+                   :valid="this.validate(formSubmitted, v$.form.name.uz.$error)"
+                   :valid-text="this.validateText(v$.form.name.uz)"
+      />
 
       <label :for="form.name.en" class="form-control-label"
       >{{ $t('subjects.name_en') }}</label
       >
       <argon-input v-model="form.name.en" type="text"
-                   :placeholder="$t('subjects.name_en')"/>
+                   :placeholder="$t('subjects.name_en')"
+                   :valid="this.validate(formSubmitted, v$.form.name.en.$error)"
+                   :valid-text="this.validateText(v$.form.name.en)"/>
 
       <hr class="horizontal dark"/>
 
@@ -50,10 +56,15 @@ import { server } from "../../../config";
 import { mapActions } from "vuex";
 import SubjectIcons from "./SubjectIcons";
 import ArgonSwitch from "@/components/ArgonSwitch";
+import useValidate from "@vuelidate/core";
+import { useToast } from "vue-toastification";
+import { required } from "@vuelidate/validators";
+import utilsMixin from "@/mixins/utilsMixin";
 
 export default {
   name: "AddSubjectForm",
   components: { SubjectIcons, ArgonInput, ArgonButton, ArgonSwitch },
+  mixins: [utilsMixin],
   data() {
     return {
       form: {
@@ -65,6 +76,20 @@ export default {
         group: false,
         icon: 'guitar'
       },
+      v$: useValidate(),
+      toast: useToast(),
+      formSubmitted: false,
+    }
+  },
+  validations() {
+    return {
+      form: {
+        name: {
+          ru: { required },
+          uz: { required },
+          en: { required }
+        }
+      }
     }
   },
   methods: {
@@ -72,9 +97,17 @@ export default {
       addSubjectToArray: 'subjects/addSubjectToArray'
     }),
     async addSubject() {
+      this.v$.$validate();
+
+      this.formSubmitted = true;
+      if (this.v$.$error) {
+        return;
+      }
+
       const data = this.form;
       const res = await axios.post(server.URL + '/api/subjects/create', data);
-      this.clearForm()
+      this.toast.success(this.$t('notifications.add_subject'))
+      await this.$router.push(`/subjects/${res.subject.id}`);
     },
     clearForm() {
       this.form = {
